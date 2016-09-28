38fdf77af4fbc3bb51a0d6767c92ee865c86e2be
Michael Rivera <mrivera.lee@gmail.com>
2016-09-27 23:34:46 -0400

    [Lab05] Solution

--- exercises/lab05/CustomDrawing/app/src/main/java/com/mikeriv/ssui_2016/customdrawing/CustomDrawingView.java
+++ exercises/lab05/CustomDrawing/app/src/main/java/com/mikeriv/ssui_2016/customdrawing/CustomDrawingView.java
@@ -13,41 +13,114 @@ import android.util.AttributeSet;
 import android.view.View;
 
 /**
- * Custom view class to drawing on a canvas
+ * TODO: document your custom view class.
  */
 public class CustomDrawingView extends View {
 
+    private Paint mCirclePaint;
+    private Paint mRectPaint;
+
     public CustomDrawingView(Context context) {
         super(context);
+        init();
     }
 
     public CustomDrawingView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        init();
     }
 
     public CustomDrawingView(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
+        init();
     }
 
+    private void init() {
+        Context context = getContext();
+        int circleColor =  ContextCompat.getColor(context, android.R.color.holo_blue_light);
+        int rectColor =  ContextCompat.getColor(context, android.R.color.holo_red_dark);
+
+        mCirclePaint = new Paint();
+        mCirclePaint.setColor(circleColor);
+
+        mRectPaint = new Paint();
+        mRectPaint.setColor(rectColor);
+    }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-
+        // If we had padding we would subtract it here
         int contentWidth = getWidth();
         int contentHeight = getHeight();
-        /* TODO implement drawing a *red* circle at (500, 500) with radius 300 relative to the main view's reference frame
-         * then draw a *blue* rectangle at (150, 150) with width: 100, height 150 relative to the circle's reference frame
-         */
+
+        // In this example, we are going to draw a view hierarchy with this
+        // CustomDrawingView as the parent node, and it will contain child that is
+        // a rectangle. That rectangle will have a child that is a circle.
+        //       CustomDrawingView (Root Node)
+        //                    \
+        //                 Rectangle
+        //                     \
+        //                  Circle
 
         // For each visual element, we want to:
-        // 1) Clip to the elements drawing bounds,
-        // 2) Translate to element's frame of reference such that drawing it puts it at 0,0
+        // 1) Clip to its drawing bounds,
+        // 2) Translate to element's origin such that it is at 0,0 when we draw
         // 3) Draw the element
-        // 4) Repeat steps for children, and children's children, etc..
-        // 5) Back out of the clipping to reset drawing region of current child
-        // 6) Back out of the translation to reset the drawing location of current child to reference of the parent
+        // 4) Repeat steps for children and their children etc.
+        // 5) Back out of the clipping to reset drawing region
+        // 6) Back out of the translation to reset the drawing location
+        int rectStartX = 100;
+        int rectStartY = 100;
+        int rectWidth = 300;
+        int rectHeight = 500;
+        canvas.save();
+        canvas.clipRect(
+                rectStartX,
+                rectStartY,
+                rectStartX + rectWidth,
+                rectStartY + rectHeight);
+        canvas.translate(rectStartX, rectStartY);
 
+        // Draw the visual element
+        canvas.drawRect(0, 0, rectWidth, rectHeight, mRectPaint);
+
+        // Save state to be at the rectangle's clipping and translation state
+        canvas.save();
+
+        // Draw the children of the visual element
+        int circleRadius = 30;
+        int circleStartX = 40;
+        int circleStartY = 40;
+
+        // Clip to bound of child
+        canvas.clipRect(
+                circleStartX,
+                circleStartY,
+                circleStartX + 2 * circleRadius,
+                circleStartY + 2 * circleRadius); // Bounding box around circle
+        // Translate to coords of child (the circle)
+        canvas.translate(circleStartX, circleStartY);
+        // We don't need to save the circle's state, if the circle doesn't have children
+        // Draw
+        canvas.drawCircle(circleRadius, circleRadius, circleRadius, mCirclePaint);
+        // We are done with the circle, so let's go back to it's parent's state (the rectangle)
+        // Canvas.restore() resets the canvas's state to when the *last*  canvas.save() operation
+        // was called. In our example, this is the equivalent of doing
+        // canvas.translate(-circleStartX, -circleStartY);
+        // canvas.clipRect(
+        //      rectStartX,
+        //      rectStartY,
+        //      rectStartX + rectWidth,
+        //      rectStartY + rectHeight,
+        //      Region.Op.REPLACE);
+        canvas.restore();
+
+        // We have nothing left to draw that is a child of rectangle
+        // Restore to the root node
+        canvas.restore();
+
+        // We're done - yay!
     }
 
 }
